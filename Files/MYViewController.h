cc2e1cb154dfab3ac997181a0829984356118dbd
wei <hongten@software-128.mitake.com.tw>
2017-11-02 10:01:38 +0800

    add miss files

---
+++ Files/MYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYViewController.h
+//  lib
+//
+//  Created by 金融研發一部-柯昊瑋 on 2017/11/1.
+//  Copyright © 2017年 金融研發一部-柯昊瑋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYViewController : UIViewController
+
+@end
---
+++ Files/MYViewController.m
@@ -0,0 +1,37 @@
+//
+//  MYViewController.m
+//  lib
+//
+//  Created by 金融研發一部-柯昊瑋 on 2017/11/1.
+//  Copyright © 2017年 金融研發一部-柯昊瑋. All rights reserved.
+//
+
+#import "MYViewController.h"
+
+@interface MYViewController ()
+
+@end
+
+@implementation MYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
